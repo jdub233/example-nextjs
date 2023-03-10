fb25aee918a3367051506c012bd8001d8f38a001
Jonathan Williams <tamlin@mac.com>
2023-03-10 15:12:16 -0500

    remove some boilerplate

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,15 +17,10 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Here I am more editing&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
               By{' '}
               <Image
                 src="/vercel.svg"
@@ -35,28 +30,10 @@ export default function Home() {
                 height={24}
                 priority
               />
-            </a>
           </div>
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
         </div>
 
         <div className={styles.grid}>
